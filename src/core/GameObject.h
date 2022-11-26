7203216201e0a253d20d923a89438930f0ca7b12
fcole90 <fcole90@gmail.com>
2022-11-26 16:23:17 +0100

    Allow ABC with default virtual destructor

--- src/core/GameObject.h
+++ src/core/GameObject.h
@@ -2,16 +2,8 @@
 #define LNGAME_OBJECT_H
 
 class GameObject {
-protected:
-  // GameObject() = default; // 0. Default constructor
 public:
-  // virtual ~GameObject() = default;                   // 1. Destructor
-  // GameObject(const GameObject &other) = default;     // 2. Copy constructor
-  // GameObject(GameObject &&other) noexcept = default; // 3. Move constructor
-  // GameObject &
-  // operator=(const GameObject &other) = default;       // 4. Copy assignment
-  // GameObject &operator=(GameObject &&other) noexcept; // 5. Move assignment
-
+  virtual ~GameObject() = default;
   virtual int onUpdate(int eps) = 0;
 };
 
